187d488372b2d7b63ed7c2ac44721bada444a2ed
Vivek <vivek.selvarajan@people10.com>
2018-08-06 17:48:56 +0530

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { BrowseComponent} from './browse/browse.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { FooterComponent } from './footer/footer.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+
+
+
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    BrowseComponent,
+    LoginComponent,
+    SignupComponent,
+    WelcomeComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: '', redirectTo: 'welcome' , pathMatch: 'full' },
+      { path: 'welcome', component: WelcomeComponent },
+      { path: 'browse', component: BrowseComponent },
+      { path: 'login', component: LoginComponent },
+      { path: 'signup', component: SignupComponent }
+    ])
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
