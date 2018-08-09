b13351603725b220e71016fcaa3e3ba8042ae31e
vivek selvarajan <vivek.selvarajan@people10.com>
2018-08-09 23:52:37 +0530

    images renamed

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+
 
 @Component({
   selector: 'app-signup',
